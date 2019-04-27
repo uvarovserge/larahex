352db87382a7fb127bd22cdd4a22f369774bb060
Serge Uvarov <hi@uvarovconsulting.com>
2019-04-27 20:24:10 +0300

    Initial commit

---
+++ helpers/precision_helpers.php
@@ -0,0 +1,100 @@
+<?php
+
+use Litipk\BigNumbers\Decimal;
+
+define('CRYPTO_PRECISION', 8);
+define('FIAT_PRECISION', 2);
+define('PRECISION', CRYPTO_PRECISION);
+
+define('DEC_SMALLEST_CRYPTO_AMOUNT', decpow(10, -CRYPTO_PRECISION));
+define('DEC_SMALLEST_FIAT_AMOUNT', decpow(10, -FIAT_PRECISION));
+
+function dec($n, $precision = PRECISION) {
+    if ($n === NULL) return Decimal::create(0, $precision);
+    return ($n instanceof Decimal) ? $n : Decimal::create($n, $precision);
+}
+
+function decvalid($value){
+    return \preg_match(Decimal::CLASSIC_DECIMAL_NUMBER_REGEXP, $value, $captures) === 1;
+}
+
+function decadd($a, $b) {
+    return dec($a)->add(dec($b), PRECISION)->asFloat();
+}
+function decsub($a, $b) {
+    return dec($a)->sub(dec($b), PRECISION)->asFloat();
+}
+/**
+ * @deprecated
+ */
+function decdiv($a, $b) {
+//    throw new \Exception('Better not use decdiv, it will provide inconsistent results.');
+    return dec($a)->div(dec($b), PRECISION)->asFloat();
+}
+function decmul($a, $b) {
+    return dec($a)->mul(dec($b), PRECISION)->asFloat();
+}
+function decpow($a, $b) {
+    return dec($a)->pow(dec($b), PRECISION)->asFloat();
+}
+
+define('DECCOMP_LEFT_IS_GREATER', 1);
+define('DECCOMP_RIGHT_IS_LESS', 1);
+define('DECCOMP_LEFT_IS_LESS', -1);
+define('DECCOMP_RIGHT_IS_GREATER', -1);
+define('DECCOMP_EQUAL', 0);
+function deccomp($a, $b) {
+    return dec($a)->comp(dec($b));
+}
+function decequal($a, $b) {
+    return deccomp($a, $b) === DECCOMP_EQUAL;
+}
+function deczero($a) {
+    return dec($a)->isZero(PRECISION);
+}
+function decnegative($a) {
+    return dec($a)->isNegative();
+}
+function decpositive($a) {
+    return dec($a)->isPositive();
+}
+function decmod($a, $b) {
+    return dec($a)->mod(dec($b))->asInteger();
+}
+
+// Input: A decimal number as a String.
+// Output: The equivalent hexadecimal number as a String.
+function dec2hex($dec) {
+    $dec = number_format($dec, 0, '', '');
+    bcscale(20);
+    $hex = '';
+    do {
+        $last = bcmod($dec, 16);
+        $hex = dechex($last).$hex;
+        $dec = bcdiv(bcsub($dec, $last), 16);
+    } while($dec>0);
+    return $hex;
+}
+
+// Input: A hexadecimal number as a String.
+// Output: The equivalent decimal number as a String.
+function hex2dec($number)
+{
+    // strip 0x
+    if (strlen($number) >= 2) if (substr($number, 0, 2) === '0x') $number = substr($number, 2);
+
+    $number = strtolower($number);
+    $decvalues = array('0' => '0', '1' => '1', '2' => '2',
+        '3' => '3', '4' => '4', '5' => '5',
+        '6' => '6', '7' => '7', '8' => '8',
+        '9' => '9', 'a' => '10', 'b' => '11',
+        'c' => '12', 'd' => '13', 'e' => '14',
+        'f' => '15');
+    $decval = '0';
+    $number = strrev($number);
+    for($i = 0; $i < strlen($number); $i++)
+    {
+        $decval = decadd(decmul(decpow('16',$i),$decvalues[$number{$i}]), $decval);
+    }
+    return $decval;
+}
---
+++ larahex.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+err() {
+    echo "Error: $1"
+    exit 1
+}
+warn() {
+    echo "Warning: $1, continuing"
+}
+replacer() {
+    php <<CODE
+<?php
+\$replace = preg_replace("$1", "$2", file_get_contents('$3')); file_put_contents('$3', \$replace);
+CODE
+    retVal=$?
+    if [ $retVal -ne 0 ]; then
+        echo "replacer() exited with non-zero code"
+        echo "Arguments:"
+        echo "1: $1"
+        echo "2: $2"
+        echo "3: $3"
+        err "replacer() exited with non-zero code"
+    fi
+}
+
+# Validation
+[[ $# -eq 0 ]] && err "No arguments supplied"
+[[ -z "$1" ]] && echo "No project name supplied"
+
+PROJECTNAME="$1"
+[[ "${PROJECTNAME//[0-9A-Za-z_]/}" = "" ]] || err "Only alphanumeric characters and underscore allowed in project name"
+
+# Create empty laravel app
+composer global update || err "Failed to global-update composer"
+composer global require "laravel/installer" || err "Failed to install laravel/installer package"
+composer create-project laravel/laravel $PROJECTNAME || err "Failed to create laravel/laravel project"
+
+echo "Entering project directory $PROJECTNAME" && cd $PROJECTNAME
+[[ $(pwd) =~ ${PROJECTNAME} ]] || err "Failed to enter into the ./$PROJECTNAME/ directory"
+
+# Make artisan executable
+chmod +x php ./artisan || warn "Failed to set +x on ./artisan"
+
+# Install Twig
+composer require rcrowe/twigbridge || err "Failed to install rcrowe/twigbridge package"
+
+replacer "/(\* Package Service Providers...\n.*?\*\/\n)/" "\$1        TwigBridge\ServiceProvider::class,\n" config/app.php
+replacer "/('Storage'\s*?=>\s*?Illuminate.Support.Facades.Storage::class,\s*?\n)/" "\$1        'Twig' => TwigBridge\\Facade\\Twig::class,\n" config/app.php
+php artisan vendor:publish --provider="TwigBridge\ServiceProvider"
+replacer "/('facades'\s*?=>\s*?\[\s*?\n)/" "\$1            'App', 'Auth', 'Route',\n" config/twigbridge.php
+replacer "/('facades'\s*?=>\s*?\[)\s*?(],)/" "\$1'App', 'Auth', 'Route',\$2" config/twigbridge.php
+replacer "/('functions'\s*?=>\s*?\[\n)/" "\$1            '__', 'app', 'str_replace',\n" config/twigbridge.php
+replacer "/('strict_variables'\s*?=>)\s*?.*?,/" "\$1 true," config/twigbridge.php
+replacer "/('autoescape'\s*?=>)\s*?.*?,/" "\$1 true," config/twigbridge.php
+
+# Delete Vue.js scaffolding
+php artisan preset none || err "Failed to set artisan preset none"
+php artisan preset bootstrap || err "Failed to set artisan preset bootstrap"
+
+# Add authentication
+php artisan make:auth || err "Failed to apply Laravel authentication scaffolding"
+
+# Blade > Twig
+wget https://raw.githubusercontent.com/uvarovserge/blade2twig/master/blade2twig.php || err "Could not download blade2twig.php"
+php blade2twig.php || err "blade2twig.php failed"
+
+# To make ->change() available in migrations
+composer require doctrine/dbal || err "Failed to install doctrine/dbal package"
+
+# Helpers
+composer require litipk/php-bignumbers || err "Failed to install litipk/php-bignumbers package"
+wget -O app/helpers.php https://raw.githubusercontent.com/uvarovserge/larahex-helpers/master/helpers.php || err "Could not download helpers.php"
+wget -O app/precision_helpers.php https://raw.githubusercontent.com/uvarovserge/larahex-helpers/master/precision_helpers.php || err "Could not download precision_helpers.php"
+replacer '/(\"autoload\"\: {\n)/' '\$1        \"files\": [\"app/helpers.php\", \"app/precision_helpers.php\"],\n' composer.json
+composer dump-autoload || err "Something is wrong with composer"
+
+# Install js packages
+npm install
+
+echo ""
+echo "Your application is ready!"
+echo ""
+
+# TODO: should we do artisan publish everything?
